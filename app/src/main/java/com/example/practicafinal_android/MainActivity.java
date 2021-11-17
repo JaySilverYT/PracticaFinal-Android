c653d833302bf0ffc86ec319e39629eeb540b25e
JaySilverYT <david.delgador@educem.net>
2021-11-17 10:33:52 +0100

    Como la lies vas solo en el grupo :)

--- app/src/main/java/com/example/practicafinal_android/MainActivity.java
+++ app/src/main/java/com/example/practicafinal_android/MainActivity.java
@@ -2,13 +2,51 @@ package com.example.practicafinal_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageView logoHotel, logoRestaurant, logoParking, logoPelis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logoHotel = findViewById(R.id.logoHotel);
+        logoHotel.setOnClickListener(this);
+
+        logoRestaurant = findViewById(R.id.logoRestaurant);
+        logoRestaurant.setOnClickListener(this);
+
+        logoParking = findViewById(R.id.logoParking);
+        logoParking.setOnClickListener(this);
+
+        logoPelis = findViewById(R.id.logoPelis);
+        logoPelis.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == logoHotel.getId())
+        {
+            startActivity(new Intent(this, hotels.class));
+        }
+        else if (v.getId() == logoRestaurant.getId())
+        {
+            startActivity(new Intent(this, Restaurants.class));
+        }
+        else if (v.getId() == logoParking.getId())
+        {
+            startActivity(new Intent(this, parking.class));
+        }
+        else if (v.getId() == logoPelis.getId())
+        {
+            startActivity(new Intent(this, movies.class));
+        }
     }
 }
---
+++ app/src/main/java/com/example/practicafinal_android/parking.java
@@ -0,0 +1,14 @@
+package com.example.practicafinal_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class parking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parking);
+    }
+}
